33eb9f473cd5d7a6c654663285607c1f058cd03c
geffk2 <arsmax3@gmail.com>
2019-07-10 10:36:01 +0700

    changed sprites and tiles

--- Server/src/main/java/websocket/main.kt
+++ Server/src/main/java/websocket/main.kt
@@ -6,6 +6,6 @@ import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) = runBlocking{
     LogManager.enable()
-    val srv = Server(GameAPI());
+    val srv = Server(GameAPI())
 //    srv.join()
 }
