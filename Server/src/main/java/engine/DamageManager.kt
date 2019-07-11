577927973c789658828f09cfdeffb2a17ab01313
S1lur <kma03.12.2002@gmail.com>
2019-07-11 15:44:37 +0700

    Added hp regen, shot cooldown, replaced entityHPs and max-||- with profiles

--- Server/src/main/java/engine/DamageManager.kt
+++ Server/src/main/java/engine/DamageManager.kt
@@ -1,66 +1,116 @@
 package engine
 
-class DamageManager {
-    private val entityHPs: MutableMap<Int, Int> = emptyMap<Int, Int>().toMutableMap()
-    private val entityMaxHPs: MutableMap<Int, Int> = emptyMap<Int, Int>().toMutableMap()
+import kotlin.math.min
 
+data class Profile(
+    var maxSpeed: Float = 5f,
+    var curHP: Int = 280,
+    var maxHP: Int = 280,
+    var hpRegen: Int = 1,
+    var escapeTimer: Int = -1,
+    var hpTimer: Int = -1,
+    var damage: Int = 30,
+    var shotCooldown: Int = 60,
+    var leftShotTimer: Int = 0,
+    var rightShotTimer: Int = 0
+)
+
+
+const val MAXESCAPETICKS = 150
+const val MAXHPTICKS = 60
+
+class DamageManager {
+    private val profiles: MutableMap<Int, Profile> = emptyMap<Int, Profile>().toMutableMap()
     val collisionDamage = 30
     val bulletDamage = 50
-    private val maxHP = 280
+
+    fun update(escapedPlayers: List<Int>): List<Int> {
+        val deadPlayers = mutableListOf<Int>()
+        for (id in profiles.keys) {
+            val profile = profiles[id]
+            if (id in escapedPlayers) {
+                if (profile!!.escapeTimer == -1) profile.escapeTimer = 1
+                else profile.escapeTimer++
+                if (profile.escapeTimer > MAXESCAPETICKS) deadPlayers.add(id)
+            } else if (profile!!.escapeTimer != -1) profile.escapeTimer = -1
+            if (profile.hpTimer == 0 && profile.curHP < profile.maxHP) profile.curHP =
+                min(profile.curHP + profile.hpRegen, profile.maxHP)
+            else profile.hpTimer--
+            if (profile.leftShotTimer != 0) profile.leftShotTimer--
+            if (profile.rightShotTimer != 0) profile.rightShotTimer--
+        }
+        return deadPlayers.toList()
+    }
 
     private fun setHP(id: Int, hp: Int) {
-        if (id !in entityHPs.keys) return
-        if (hp > entityMaxHPs[id]!!) {
-            entityHPs[id] = entityMaxHPs[id]!!
+        if (id !in profiles.keys) return
+        if (hp > profiles[id]!!.maxHP) {
+            profiles[id]!!.curHP = profiles[id]!!.maxHP
             return
         }
-        entityHPs[id] = hp
+        profiles[id]!!.curHP = hp
     }
 
     fun refreshPlayer(id: Int) {
-        if (id !in entityHPs.keys) return
-        setHP(id, entityMaxHPs[id]!!)
+        if (id !in profiles.keys) return
+        profiles[id] = Profile()
     }
 
     fun setMaxHP(id: Int, maxHP: Int) {
-        if (id !in entityMaxHPs.keys) return
-        entityMaxHPs[id] = maxHP
-        entityHPs[id] = entityMaxHPs[id]!!
+        if (id !in profiles.keys) return
+        val oldMax = profiles[id]!!.maxHP
+        profiles[id]!!.maxHP = maxHP
+        profiles[id]!!.curHP = (profiles[id]!!.curHP * maxHP.toFloat() / oldMax.toFloat()).toInt()
     }
 
     fun assignHP(id: Int) {
-        if (id in entityHPs.keys) return
-        entityMaxHPs[id] = maxHP
-        entityHPs[id] = entityMaxHPs[id]!!
+        if (id in profiles.keys) return
+        profiles[id] = Profile()
     }
 
     fun dealDamage(id: Int, damage: Int): DeathState {
-        if (id !in entityHPs.keys) return DeathState.NONE
-        entityHPs[id] = entityHPs[id]!! - damage
-
-        if (entityHPs[id]!! <= 0f) {
+        if (id !in profiles.keys) return DeathState.NONE
+        profiles[id]!!.curHP -= damage
+        profiles[id]!!.hpTimer = MAXHPTICKS
+        if (profiles[id]!!.curHP <= 0) {
             return DeathState.DEAD
         }
         return DeathState.ALIVE
     }
 
     fun getHPbyId(id: Int): Int {
-        if (id !in entityHPs.keys) return -1
-        return entityHPs[id]!!
+        if (id !in profiles.keys) return -1
+        return profiles[id]!!.curHP
     }
 
     fun getMaxHPbyId(id: Int): Int {
-        if (id !in entityMaxHPs.keys) return -1
-        return entityMaxHPs[id]!!
+        if (id !in profiles.keys) return -1
+        return profiles[id]!!.maxHP
+    }
+
+    fun getMaxSpeedById(id: Int): Float {
+        if (id !in profiles.keys) return -1f
+        return profiles[id]!!.maxSpeed
     }
 
     fun removeEntity(id: Int) {
-        if (id !in entityHPs.keys) return
-        for (key in entityHPs.keys) {
-            if (entityHPs[key] == id) {
-                entityHPs.remove(key)
-                return
-            }
+        if (id !in profiles.keys) return
+        profiles.remove(id)
+    }
+
+    fun checkShotCooldown(id: Int, side: Int): Boolean {
+        if (id !in profiles.keys) return true
+        return when (side) {
+            1 -> profiles[id]!!.leftShotTimer != 0
+            else -> profiles[id]!!.rightShotTimer != 0
+        }
+    }
+
+    fun goOnCooldown(id: Int, side: Int) {
+        if (id !in profiles.keys) return
+        return when (side) {
+            1 -> profiles[id]!!.leftShotTimer = profiles[id]!!.shotCooldown
+            else -> profiles[id]!!.rightShotTimer = profiles[id]!!.shotCooldown
         }
     }
 }
